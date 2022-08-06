9640be5bbfb0f614da8a84562d8391988660cf7c
Anuj Kondhalkar <anujkondhalkar@outlook.com>
2022-08-06 22:35:42 +0530

    Task --> ListView

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
 
-def taskList(request):
-    return HttpResponse("To Do List")
+class TaskList(ListView):
+    # it will be rendered to html
+    model = Task
+    # represent query list i.e. tasks it will go to task_list.html as object_list
+    context_object_name = 'tasks'
