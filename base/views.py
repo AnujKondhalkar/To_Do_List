c9f37bd1e611513f39e43ef3b8bfb6df58a92b28
Anuj Kondhalkar <anujkondhalkar@outlook.com>
2022-08-12 17:49:10 +0530

    Create item view set ---- and some comments were added

--- base/views.py
+++ base/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 
 class TaskList(ListView):
     # it will be rendered to html
-    model = Task
+    model = Task  # the db classes we have made in models.py and those were migrated
     # represent query list i.e. tasks it will go
     # to task_list.html as object_list
     context_object_name = 'tasks'
@@ -18,3 +21,9 @@ class TaskDetail(DetailView):
     context_object_name = 'task'
     template_name = 'base/task.html'  # previously task_detail.html
     # normally task_detail.hmtl name is made as Task from model name and detail from this class name
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'  # e.g. ['title','description']
+    success_url = reverse_lazy('tasks')  # 'tasks is url name'
